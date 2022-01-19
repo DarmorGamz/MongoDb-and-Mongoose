a768b2ba3aaf321c3a4e68b7a939714ca8e598d8
Darren Morrison <darren09@live.ca>
2022-01-19 02:09:38 +0000

    Created method FindOneByFood to query MongoDb for only one matching Food

--- myApp.js
+++ myApp.js
@@ -42,7 +42,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
 
 const findPersonById = (personId, done) => {
